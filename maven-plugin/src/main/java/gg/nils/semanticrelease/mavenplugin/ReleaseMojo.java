00749940af7d122c23262b93f0b463d3c946936c
thenilsdev <contact@ni.ls>
2021-06-21 19:37:22 +0200

    feat: Update semantic-release:release to fail when something is wrong, so the ci can handle this

--- maven-plugin/src/main/java/gg/nils/semanticrelease/mavenplugin/ReleaseMojo.java
+++ maven-plugin/src/main/java/gg/nils/semanticrelease/mavenplugin/ReleaseMojo.java
@@ -30,9 +30,15 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidTagNameException;
+import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import java.io.IOException;
 
 @Mojo(name = ReleaseMojo.GOAL, defaultPhase = LifecyclePhase.INITIALIZE, threadSafe = true)
 public class ReleaseMojo extends AbstractMojo {
@@ -46,47 +52,42 @@ public class ReleaseMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
         Log logger = this.getLog();
 
-        try {
-            FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder()
-                    .findGitDir(this.project.getBasedir());
+        FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder()
+                .findGitDir(this.project.getBasedir());
 
-            if (repositoryBuilder.getGitDir() == null) {
-                logger.error(".git dir not found!");
-                return;
+        if (repositoryBuilder.getGitDir() == null) {
+            logger.error(".git dir not found!");
+            return;
+        }
+
+        try (Repository repository = repositoryBuilder.build()) {
+            Git git = new Git(repository);
+
+            SemanticReleaseConfig config = new DefaultSemanticReleaseConfig();
+
+            VersionControlProvider versionControlProvider = new GitVersionControlProvider(config, git);
+
+            if (!versionControlProvider.getCurrentBranch().getName().equals("master")
+                    && !versionControlProvider.getCurrentBranch().getName().equals("main")) {
+                throw new MojoFailureException("Only master and main branches can be released!");
             }
 
-            try (Repository repository = repositoryBuilder.build()) {
-                Git git = new Git(repository);
-
-                SemanticReleaseConfig config = new DefaultSemanticReleaseConfig();
-
-                VersionControlProvider versionControlProvider = new GitVersionControlProvider(config, git);
-
-                if(!versionControlProvider.getCurrentBranch().getName().equals("master")
-                        && !versionControlProvider.getCurrentBranch().getName().equals("main")) {
-                    logger.error("Only master and main branches can be released!");
-                    return;
-                }
-
-                if(versionControlProvider.hasUncommittedChanges()) {
-                    logger.error("This branch has uncommitted changes, cannot release!");
-                    return;
-                }
-
-                if(versionControlProvider.getLatestVersion().equals(versionControlProvider.getNextVersion())) {
-                    logger.error("There were no changes that would require a release!");
-                    return;
-                }
-
-                Ref ref = git.tag()
-                        .setName(versionControlProvider.getNextVersion().toString())
-                        .setAnnotated(false)
-                        .call();
-
-                logger.info("New version: " + ref.getName());
+            if (versionControlProvider.hasUncommittedChanges()) {
+                throw new MojoFailureException("This branch has uncommitted changes, cannot release!");
             }
-        } catch (Throwable t) {
-            throw new MojoFailureException("semantic-release failed", t);
+
+            if (versionControlProvider.getLatestVersion().equals(versionControlProvider.getNextVersion())) {
+                throw new MojoFailureException("There were no changes that would require a release!");
+            }
+
+            Ref ref = git.tag()
+                    .setName(versionControlProvider.getNextVersion().toString())
+                    .setAnnotated(false)
+                    .call();
+
+            logger.info("New version: " + ref.getName());
+        } catch (IOException | GitAPIException e) {
+            e.printStackTrace();
         }
     }
 }
