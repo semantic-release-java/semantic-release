bf0b3e348fadaf4d2831097aeb9e84cfbf6558c1
thenilsdev <contact@ni.ls>
2021-06-23 13:43:57 +0200

    feat: Improve git describe parsing

--- src/main/java/gg/nils/semanticrelease/versioncontrol/git/GitVersionControlProvider.java
+++ src/main/java/gg/nils/semanticrelease/versioncontrol/git/GitVersionControlProvider.java
@@ -23,6 +23,7 @@ import gg.nils.semanticrelease.error.SemanticReleaseException;
 import gg.nils.semanticrelease.versioncontrol.VersionControlProviderImpl;
 import gg.nils.semanticrelease.versioncontrol.git.converter.*;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -36,6 +37,7 @@ import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 public class GitVersionControlProvider extends VersionControlProviderImpl {
 
     private final Git git;
@@ -111,15 +113,9 @@ public class GitVersionControlProvider extends VersionControlProviderImpl {
             if (call == null)
                 return null;
 
-            Matcher matcher = Pattern.compile("(.+)-(.+)-(.+)$").matcher(call);
+            String version = this.extractVersion(call);
 
-            String version;
-
-            if (!matcher.find()) {
-                version = call;
-            } else {
-                version = matcher.group(1);
-            }
+            log.info("transformed describe result from {} to {}", call, version);
 
             Optional<Tag> optionalTag = this.getTags().stream()
                     .filter(tag -> tag.getName().equals(version))
@@ -129,6 +125,22 @@ public class GitVersionControlProvider extends VersionControlProviderImpl {
         } catch (GitAPIException e) {
             throw new SemanticReleaseException("Could not get tags", e);
         }
+    }
+
+    private String extractVersion(String describeInput) {
+        Matcher matcher1 = Pattern.compile("(.+)-(.+)-(.+)$").matcher(describeInput);
+
+        if(matcher1.find()) {
+            return matcher1.group(1);
+        }
+
+        Matcher matcher2 = Pattern.compile("(.+)-([0-9a-f]{40})$").matcher(describeInput);
+
+        if(matcher2.find()) {
+            return matcher2.group(1);
+        }
+
+        return describeInput;
     }
 
     @Override
